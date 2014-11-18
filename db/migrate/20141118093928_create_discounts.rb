9999555407ac1c65e4739771611e9f72ff743a42
Nate <primesitemedia@gmail.com>
2014-11-18 17:54:14 +0800

    setup Discount / Restaurant association

--- db/migrate/20141118093928_create_discounts.rb
+++ db/migrate/20141118093928_create_discounts.rb
@@ -1,7 +1,7 @@
 class CreateDiscounts < ActiveRecord::Migration
   def change
     create_table :discounts do |t|
-      t.integer :restaurant_id
+      t.references :restaurant_id
       t.time :start_time
       t.integer :amount
 
