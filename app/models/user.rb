75434127b95f96d17c5b239a9febb38f0dad2bd0
taliascotchbrook <taliaisontheweb@gmail.com>
2014-11-21 16:05:03 +0800

    more authentication added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
       user.email = auth.info.email
-      user.passwork = Devise.friendly_token[0,20]
-      user.name = auth.info.name
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name #if there is a name
+      user.image = auth.info.image
       user.uid = auth.uid
     end
   end
