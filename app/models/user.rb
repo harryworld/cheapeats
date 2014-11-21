edfab504fdea00275ed763fa64baaeb8b9f91dd4
taliascotchbrook <taliaisontheweb@gmail.com>
2014-11-21 15:58:28 +0800

    facebook authentication continued. Add key & secret, and controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.email = auth.info.email
+      user.passwork = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.uid = auth.uid
+    end
+  end
+
 end
